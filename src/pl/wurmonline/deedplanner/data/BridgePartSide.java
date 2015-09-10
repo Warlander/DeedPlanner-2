a459ae1c6b26d6b3dabcf7905418f924e18d3010
Warlander <mjcc@tlen.pl>
2015-09-10 04:18:13 +0200

    Revert "Initial bridges commit (broken atm due to unfinished part of code)."

--- src/pl/wurmonline/deedplanner/data/BridgePartSide.java
+++
@@ -1,5 +0,0 @@
-package pl.wurmonline.deedplanner.data;
-
-public enum BridgePartSide {
-    NARROW, CENTER, LEFT, RIGHT;
-}
--- src/pl/wurmonline/deedplanner/data/BridgePartType.java
+++
@@ -1,5 +0,0 @@
-package pl.wurmonline.deedplanner.data;
-
-public enum BridgePartType {
-    ABUTMENT, BRACING, CROWN, DOUBLE_BRACING, DOUBLE_ABUTMENT, SUPPORT, EXTENSION;
-}
