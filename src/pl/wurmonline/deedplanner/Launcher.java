84e73208e49a2c21275da27c94fcfa98cca9fafe
Warlander <warlander@wurmonline.com>
2016-06-25 00:47:02 +0200

    Added checking if program is launched from .zip file.
    This was a problem due to many people trying to launch the program
    without unzipping it first.

--- src/pl/wurmonline/deedplanner/Launcher.java
+++ src/pl/wurmonline/deedplanner/Launcher.java
@@ -2,6 +2,10 @@ package pl.wurmonline.deedplanner;
 
 import java.io.*;
 import java.util.Locale;
+import javax.media.opengl.GL;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 import pl.wurmonline.deedplanner.forms.Loading;
 import pl.wurmonline.deedplanner.util.*;
 
@@ -31,6 +35,27 @@ public class Launcher {
         }
         
         SwingUtils.setLookAndFeel(Properties.lookAndFeel);
+        
+        //before continuing, check if libraries are accessable and catch exception otherwise
+        try {
+            Class anyExternalLibraryClass = GL.class;
+        }
+        catch (NoClassDefFoundError error) {
+            error.printStackTrace(System.err);
+            JOptionPane.showMessageDialog(null,
+                                          "Critical DeedPlanner error occured.\n" +
+                                          "Cannot find external libraries bundled with the program.\n" +
+                                          "This usually happens in two cases:\n" +
+                                          "1. Program is not unzipped from the .zip archive after download.\n" +
+                                          "2. Jar file used to run this program is moved somewhere else\n" +
+                                          "without moving all other files in program directory.\n\n" +
+                                          "If this message appears in other situations than mentioned,\n" +
+                                          "please copy content of ErrorLog.txt file and report\n" +
+                                          "the error in a program thread.",
+                                          "DeedPlanner critical error",
+                                          JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
         
         new Loading();
     }
