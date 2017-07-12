9a81e3ad54059080e3c59864474187637d1b7c00
Warlander <warlander@wurmonline.com>
2017-07-12 02:22:12 +0200

    2d frustum culling fix, code quality improvements

--- src/pl/wurmonline/deedplanner/graphics/UpCamera.java
+++ src/pl/wurmonline/deedplanner/graphics/UpCamera.java
@@ -8,6 +8,8 @@ import pl.wurmonline.deedplanner.input.*;
 
 public final class UpCamera {
     
+    private static final int OFFSCREEN_TILES_RENDERED = 3;
+    
     public float x=0;
     public float y=0;
     
@@ -26,30 +28,31 @@ public final class UpCamera {
     public void update(Mouse mouse, Keybindings keybindings) {
         Map map = panel.getMap();
         
-        int currX = (int)(x/4);
-        int currZ = (int)(y/4);
-        Globals.visibleDownX = currX-5;
-        Globals.visibleUpX = currX+Properties.scale*2;
-        Globals.visibleDownY = currZ-5;
-        Globals.visibleUpY = currZ+Properties.scale*2;
+        float tileScaler = (float) panel.getWidth() / panel.getHeight();
         
-        float tileScaler = (float)panel.getWidth()/(float)panel.getHeight();
-        float tileSize = (float)panel.getHeight()/Properties.scale/4;
-        float tileX = ((mouse.x+x*tileSize)/((float)panel.getWidth()/Properties.scale/tileScaler));
-        float tileY = (((panel.getHeight()-mouse.y)+y*tileSize)/((float)panel.getHeight()/Properties.scale));
+        int currX = (int) (x / 4);
+        int currZ = (int) (y / 4);
+        Globals.visibleDownX = currX - OFFSCREEN_TILES_RENDERED;
+        Globals.visibleUpX = currX + (int) (Properties.scale * tileScaler) + OFFSCREEN_TILES_RENDERED;
+        Globals.visibleDownY = currZ - OFFSCREEN_TILES_RENDERED;
+        Globals.visibleUpY = currZ + Properties.scale + OFFSCREEN_TILES_RENDERED;
+        
+        float tileSize = (float) panel.getHeight() / Properties.scale / 4;
+        float tileX = ((mouse.x + x * tileSize) / ((float) panel.getWidth() / Properties.scale / tileScaler));
+        float tileY = (((panel.getHeight() - mouse.y) + y * tileSize) / ((float) panel.getHeight() / Properties.scale));
         
         double keyboardFraction = Properties.keyboardFractionUp;
         if (keybindings.hold(Keybindings.UP_SPEED_MOD1)) {
-            keyboardFraction*=Properties.mod1Fpp;
+            keyboardFraction *= Properties.mod1Fpp;
         }
         else if (keybindings.hold(Keybindings.UP_SPEED_MOD2)) {
-            keyboardFraction*=Properties.mod2Fpp;
+            keyboardFraction *= Properties.mod2Fpp;
         }
         
         currX = (int) tileX;
         int currY = (int) tileY;
         
-        if (currX>=0 && currX<map.getWidth() && currY>=0 && currY<map.getHeight()) {
+        if (currX >= 0 && currX < map.getWidth() && currY >= 0 && currY < map.getHeight()) {
             tile = map.getTile(currX, currY);
             xMap = tileX;
             yMap = tileY;
@@ -60,90 +63,61 @@ public final class UpCamera {
             tile = null;
         }
         
-        if (keybindings.pressed(Keybindings.UP_SCALE_MORE)) {
-            if (Properties.scale>5) {
+        if (mouse.scrollDown || keybindings.pressed(Keybindings.UP_SCALE_MORE)) {
+            if (Properties.scale > 5) {
                 Properties.scale--;
             }
         }
-        else if (keybindings.pressed(Keybindings.UP_SCALE_LESS)) {
-            if (Properties.scale<40) {
+        else if (mouse.scrollUp || keybindings.pressed(Keybindings.UP_SCALE_LESS)) {
+            if (Properties.scale < 40) {
                 Properties.scale++;
             }
-        }
-        
-        if (mouse.scrollDown) {
-            if (Properties.scale>5) {
-                Properties.scale--;
-            }
-        }
-        else if (mouse.scrollUp) {
-            if (Properties.scale<40) {
-                Properties.scale++;
-            }
-        }
-        else {
-            if ((mouse.scrollUp ^ mouse.scrollDown) && !(keybindings.pressed(Keybindings.UP_SPEED_MOD1) || keybindings.pressed(Keybindings.UP_SPEED_MOD2))) {
-//                JScrollPane pane = (JScrollPane)HouseCalc.contextPane.getSelectedComponent();
-//                JList list = (JList)((JViewport)pane.getComponent(0)).getView();
-//                if (mouse.scrollUp) {
-//                    list.setSelectedIndex(list.getSelectedIndex()-1);
-//                }
-//                else if (mouse.scrollDown) {
-//                    list.setSelectedIndex(list.getSelectedIndex()+1);
-//                }
-            }
-        }
-        if (keybindings.pressed(Keybindings.UP_ELEVATION_DOWN)) {
-            //Mapper.floorDown();
-        }
-        else if (keybindings.pressed(Keybindings.UP_ELEVATION_UP)) {
-            //Mapper.floorUp();
         }
         
         if (mouse.pressed.middle) {
             mouse.setGrabbed(true);
         }
         else if (mouse.hold.middle) {
-            x-=mouse.diffX*Properties.mouseFractionUp;
-            y+=mouse.diffY*Properties.mouseFractionUp;
+            x -= mouse.diffX * Properties.mouseFractionUp;
+            y += mouse.diffY * Properties.mouseFractionUp;
         }
         else if (mouse.released.middle && !mouse.hold.middle) {
             mouse.setGrabbed(false);
         }
         
         if (keybindings.hold(Keybindings.UP_MOVE_UP)) {
-            y+=keyboardFraction;
+            y += keyboardFraction;
         }
         if (keybindings.hold(Keybindings.UP_MOVE_DOWN)) {
-            y-=keyboardFraction;
+            y -= keyboardFraction;
         }
         
         if (keybindings.hold(Keybindings.UP_MOVE_RIGHT)) {
-            x+=keyboardFraction;
+            x += keyboardFraction;
         }
         if (keybindings.hold(Keybindings.UP_MOVE_LEFT)) {
-            x-=keyboardFraction;
+            x -= keyboardFraction;
         }
         
-        if (y<0) {
-            y=0;
+        if (y < 0) {
+            y = 0;
         }
-        if (x<0) {
-            x=0;
+        if (x < 0) {
+            x = 0;
         }
         
-        if (y>4*(map.getHeight()-Properties.scale)) {
-            y=4*(map.getHeight()-Properties.scale);
+        if (y > 4 * (map.getHeight() - Properties.scale)) {
+            y = 4 * (map.getHeight() - Properties.scale);
         }
-        if (x>4*(map.getWidth()-Properties.scale)) {
-            x=4*(map.getWidth()-Properties.scale);
+        if (x > 4 * (map.getWidth() - Properties.scale * tileScaler)) {
+            x = 4 * (map.getWidth() - Properties.scale * tileScaler);
         }
     }
     
     public void set(GL2 g) {
         g.glMatrixMode(GL2.GL_PROJECTION);
         g.glLoadIdentity();
-        g.glOrtho(0, Properties.scale*4f*(float)panel.getWidth()/(float)panel.getHeight(), 0, Properties.scale*4, 0.001f, 8192);
+        g.glOrtho(0, Properties.scale * 4f * (float)panel.getWidth() / (float)panel.getHeight(), 0, Properties.scale * 4, 0.001f, 8192);
         g.glTranslatef(-x, -y, -4000);
         g.glMatrixMode(GL2.GL_MODELVIEW);
         g.glLoadIdentity();
