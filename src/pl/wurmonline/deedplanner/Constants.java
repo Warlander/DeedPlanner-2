aa3e823b703578bf0c30574b29b62b64fee2b405
Warlander <warlander@wurmonline.com>
2017-07-18 17:26:21 +0200

    Version bump to 2.8.8

--- src/pl/wurmonline/deedplanner/Constants.java
+++ src/pl/wurmonline/deedplanner/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     
     public static final float HEIGHT_MOD = 10f;
     
-    public static final String VERSION_STRING = "DeedPlanner 2.8.7";
+    public static final String VERSION_STRING = "DeedPlanner 2.8.8";
     
     public static final String TREE_TYPE = "tree";
     public static final String BUSH_TYPE = "bush";
